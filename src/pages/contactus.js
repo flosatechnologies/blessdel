18ac9a1e0865988da4aaaf9b584a05c230d90c16
iamkupoe <samuelkupoe1@gmail.com>
2021-10-12 17:53:47 -0400

    MenuBar: In progress

---
+++ src/pages/contactus.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ContactUs() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Contact Us</h1>
+    </div>
+  );
+}
+
+export default ContactUs;
