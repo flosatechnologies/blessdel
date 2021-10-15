9ca72b233abb61f13e05045cf4a40e3e44674cbd
Florentpop <Florentpop@gmail.com>
2021-10-15 19:45:14 +0000

    Project and Contantus & others screens

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,7 +7,7 @@ class Home extends Component {
   render() {
     return (
       <Container fluid className="bg-image image-fluid">
-        <div className="overlay">
+        <div id="main" className="overlay">
           <h3>We stand for</h3>
           <p>Integrity, Honesty, Ethical, Cost Effective and Trustworthy</p>
         </div>
