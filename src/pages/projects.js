3269ec8891e91f8ee3adb7a60e793aff19a73775
Adams <bamfoadamsf@gmail.com>
2021-10-27 18:42:06 +0000

    updated project slider

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,14 +3,14 @@ import Carousel from "react-bootstrap/Carousel";
 import "../css/projects.css";
 import testImg from "../assets/images/testimony1.jpg";
 import testImg2 from "../assets/images/testimony2.jpg";
-import "../css/imageComponent.css";
+import "../css/ImageComponent.css";
 import { Container, Col, Row } from "react-bootstrap";
-import EB2 from "../assets/images/E-Block2.jpg";
-import SM from "../assets/images/site meeting.jpg";
-import RM from "../assets/images/Minister.jpg";
-import University from "../assets/images/university-cape.jpg";
-import Ben from "../assets/images/Mr.Ben.jpg";
-import Sena from "../assets/images/Sena.jpg";
+import EB2 from "../assets/images/E-Block2-edited.jpg";
+import SM from "../assets/images/site meeting-edited.jpg";
+import RM from "../assets/images/Minister-edited.jpg";
+import University from "../assets/images/university-cape-edited.jpg";
+import Ben from "../assets/images/Mr.Ben-edited.jpg";
+import Sena from "../assets/images/Sena-edited.jpg";
 
 function Projects() {
   return (
