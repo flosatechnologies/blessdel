9151685dcaea125bbf79e51d197d46755f37ae8a
Adams <bamfoadamsf@gmail.com>
2021-11-07 15:19:06 +0000

    corrected the typo mistake

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -199,7 +199,7 @@ function Projects() {
                     <Col className="testimonialText">
                       I met Blessdel 4 years ago and upon giving my building to
                       them all I can say is they are true professionals, honest
-                      and do they work efficiently. I will therefore recommend
+                      and do their work efficiently. I will therefore recommend
                       them for anyone
                     </Col>
                   </Row>
